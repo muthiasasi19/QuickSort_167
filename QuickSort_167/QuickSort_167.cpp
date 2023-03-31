4e2745dca3735a93ea67f748cd367bb8a643bdfe
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-31 16:59:12 -0700

    menyusun array dengan quick sort

--- QuickSort_167/QuickSort_167.cpp
+++ QuickSort_167/QuickSort_167.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
 using namespace std;
 
-
 //array of integers to hold values
 int arr[20];
 int cmp_count = 0; //number of comparasion
 int mov_count = 0; // number of movement
 int n;
+
 void input() {
 	while (true)
 	{
-		cout << "masukan panjang element array :";
+		cout << "Masukan panjang element array: ";
 		cin >> n;
 
 		if (n <= 20)
 			break;
 		else
-			cout << "\n Maksimum panjang array adalah 20" << endl;
+			cout << "\nMaksimum panjang array adalah 20" << endl;
 	}
 
-	cout << "\n--------------------" << endl;
+	cout << "\n-------------------" << endl;
 	cout << "\nEnter array element" << endl;
-	cout << "\n--------------------" << endl;
+	cout << "\n-------------------" << endl;
 
 	for (int i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << ">";
 		cin >> arr[i];
 	}
-}
 
+}
 // swaps the element st index with the element at index y
+
 void swap(int x, int y)
 {
 	int temp;
-
 	temp = arr[x];
 	arr[x] = arr[y];
 	arr[y] = temp;
@@ -50,11 +50,9 @@ void q_short(int low, int high)
 	//on econtaining element less that or equal to pivot
 	// other containing element
 
-	pivot = arr[low];								// langkah 2
-
-	i = low + 1;									// langkah 3
+	i = low + 1;									// langkah 2
 	j = high;										// langkah 4
-
+	pivot = arr[low];								// langkah 2
 
 	while (i <= j)
 	{												// langkah 10
@@ -67,34 +65,42 @@ void q_short(int low, int high)
 		cmp_count++;
 	
 	// search for an element less than or equal to pivot
+
 	while ((arr[j] > pivot) && (j >= low))			// langkah 7
 	{
 		j--;										// langkah 8
 		cmp_count++;
 	}
+
 	cmp_count++;
-	// if the greater element is on the left of the element
+
 	if (i < j)										// langkah 9
+		// if the greater element is on the left of the element
 	{
 		// swap the element at index i with the element at index j
+
 		swap(i, j);
 		mov_count++;
 	}
 }
 
 // j now containt the index of the last element in the sorted list
+
 if (low < j)										// langkah 11
-{
+
 	// move the pivot to its correct position in the list
+{
 	swap(low, j);
 	mov_count++;
 } 
+
 // sort the list ojn the right of pivot using quick sort
+
 q_short(low, j - 1);								// langkah 12
 
 //	sort the list on the right of pivot uding the quick sort 
-q_short(j + 1, high);								// langkah 13
 
+q_short(j + 1, high);								// langkah 13
 } 
 
 void display() {
@@ -106,4 +112,17 @@ void display() {
 	{
 		cout << arr[i] << " ";
 	}
+	cout << "\n\nNumber of comparasion: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+} 
+
+int main()
+{ 
+	input();
+	q_short(0, n - 1);						// Sort the array using quick sort 
+	display();
+	system("pause");
+
+	return 0;
 }
+
