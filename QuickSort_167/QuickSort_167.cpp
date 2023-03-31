ff4a7619a587388e43367d7dab03cac41d5cf7db
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-03-31 16:35:56 -0700

    menampilkan perhitungan

--- QuickSort_167/QuickSort_167.cpp
+++ QuickSort_167/QuickSort_167.cpp
@@ -97,4 +97,13 @@ q_short(j + 1, high);								// langkah 13
 
 } 
 
-	
+void display() {
+	cout << "\n----------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "\n----------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
